c2af24352cf2b28be390a0d7d1cff21bb0bbd85b
zgoksu <zafergoksu00@gmail.com>
2024-02-25 21:09:48 +0000

    test: add size test for value test

--- tests/test_value.cpp
+++ tests/test_value.cpp
@@ -8,6 +8,7 @@ TEST(ValueArray, test_write_value) {
     value_array.write_value(2.1);
 
     EXPECT_THAT(value_array.get_values(), ::testing::ContainerEq(std::vector<double>{1.2, 2.1}));
+    EXPECT_EQ(value_array.size(), 2);
 }
 
 int main(int argc, char* argv[]) {
