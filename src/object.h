497985e0d661633193311e0e4b3bfba439db6ed1
zgoksu <zafergoksu00@gmail.com>
2025-09-28 03:22:18 +0100

    chore: remove comment

--- src/object.h
+++ src/object.h
@@ -24,7 +24,6 @@ struct Object {
     Object();
     Object(const ObjectType type);
     virtual ~Object();
-    // Test
 
     virtual std::string to_string() const;
     virtual bool is_falsey() const;
